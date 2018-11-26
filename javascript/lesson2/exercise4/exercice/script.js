909729d81b80af66c7306281d7b8df1de2344e22
BC029 <rainbow-life@outlook.fr>
2018-11-26 16:44:23 +0100

    Exercice 4 - Correction ( Indentation )

--- javascript/lesson2/exercise4/exercice/script.js
+++ javascript/lesson2/exercise4/exercice/script.js
@@ -2,14 +2,14 @@ var position = "Mairie des Lilas";
 
 switch (position) {
     case position = "Belleville" :
-    console.log("Tu es sur la ligne 11 ou la ligne 2");
-    break;
+        console.log("Tu es sur la ligne 11 ou la ligne 2");
+        break;
     case position = "Mairie des Lilas" :
-    console.log("Tu es sur la ligne 11");
-    break;
+        console.log("Tu es sur la ligne 11");
+        break;
     case position = "Père Lachaise" :
-    console.log("Tu es sur la ligne 3 ou 2");
-    break;
+        console.log("Tu es sur la ligne 3 ou 2");
+        break;
     default :
-    console.log("Je sais pas où tu es mec");
+        console.log("Je sais pas où tu es mec");
 }
