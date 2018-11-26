8b7d5755834af1857b10b3318495a933c5faab04
BC029 <rainbow-life@outlook.fr>
2018-11-26 15:57:36 +0100

    Exercice 4

---
+++ javascript/lesson2/exercise4/exercice/script.js
@@ -0,0 +1,15 @@
+var position = "Mairie des Lilas";
+
+switch ( position ) {
+    case position = "Belleville" :
+    console.log("Tu es sur la ligne 11 ou la ligne 2");
+    break;
+    case position = "Mairie des Lilas" :
+    console.log("Tu es sur la ligne 11");
+    break;
+    case position = "Père Lachaise" :
+    console.log("Tu es sur la ligne 3 ou 2");
+    break;
+    default :
+    console.log("Je sais pas où tu es mec");
+}
