2c502b2991aa252104baab47fc6f7a2289496105
BC029 <rainbow-life@outlook.fr>
2018-11-26 15:43:23 +0100

    Exercice 2

---
+++ javascript/lesson2/exercise2/exercice/script.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+for ( count = 0; count <= 100; count++ ) {
+    console.log('Valeur : ' + count);
+}
+for ( count = 0; count <= 100; count = count + 2 ) {
+    console.log('Valeur : ' + count);
+}
+for ( count = 100; count >= 0; count = count - 2 ) {
+    console.log('Valeur : ' + count);
+}
