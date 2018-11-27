fd484c4fd944c3e5916aee0fe5a904b87c8fe9f2
BC029 <rainbow-life@outlook.fr>
2018-11-27 17:54:03 +0100

    Exercice 3

---
+++ javascript/lesson3/exercise3/exercice/script.js
@@ -0,0 +1,6 @@
+var etage = "3ème étage"
+var porte = "Porte droite"
+var monAppartement = function(boiteAuxLettres) {
+    console.log("Etage : ", etage, ",", "Porte : ", porte, ",", "Boîte Aux Lettres : ", boiteAuxLettres);
+}
+monAppartement(3);
