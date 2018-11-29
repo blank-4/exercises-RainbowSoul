3d85504b2fd75414f4848bf2589374f033db84b8
BC029 <rainbow-life@outlook.fr>
2018-11-29 10:11:27 +0100

    Exercice 7

---
+++ javascript/lesson3/exercise7/exercice/script.js
@@ -0,0 +1,9 @@
+function reversedNum(num) {
+    return 1 / num;
+}
+console.log("L'inverse est : ", reversedNum(5));
+
+function opposedNum(num) {
+    return - num;
+}
+console.log("L'opposé est : ", opposedNum(5));
