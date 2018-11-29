cdec166022929ec47ae5c513d2cf51916b8719dc
BC029 <rainbow-life@outlook.fr>
2018-11-29 15:24:43 +0100

    Exercice  - Correction

--- javascript/lesson3/exercise3/exercice/script.js
+++ javascript/lesson3/exercise3/exercice/script.js
@@ -1,6 +1,7 @@
 var etage = "3ème étage"
 var porte = "Porte droite"
-var monAppartement = function(boiteAuxLettres) {
+var monAppartement = function() {
+    var boiteAuxLettres = "97";
     console.log("Etage : ", etage, ",", "Porte : ", porte, ",", "Boîte Aux Lettres : ", boiteAuxLettres);
 }
-monAppartement(3);
+monAppartement();
