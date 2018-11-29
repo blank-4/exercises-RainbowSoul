f0b01875e94092395a1559ad664f038878064919
BC029 <rainbow-life@outlook.fr>
2018-11-29 11:08:38 +0100

    Exercice 6 - Boucle

--- javascript/lesson3/exercise6/exercice/script.js
+++ javascript/lesson3/exercise6/exercice/script.js
@@ -1,4 +1,8 @@
 function aireCercle(num1, num2) {
-    return Math.pow(num1, num2);
+    if (num1 != num2 || num1 > num2 || num1 < num2 || num1 === num2) {
+        return num1 ** num2;
+    } else {
+        return "Error";
+    }
 }
-console.log("Le résultat est", aireCercle(2, 3));
+console.log("Le résultat est", aireCercle(6, 5));
