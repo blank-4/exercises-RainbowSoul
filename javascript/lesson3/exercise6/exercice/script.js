d7182008b99706b21c619ba7b614ad75d6ae5894
BC029 <rainbow-life@outlook.fr>
2018-11-29 16:03:39 +0100

    Exercice 6 : Simplification 1

--- javascript/lesson3/exercise6/exercice/script.js
+++ javascript/lesson3/exercise6/exercice/script.js
@@ -1,10 +1,8 @@
 function aireCercle(num1, num2) {
     if (num1 <= num2 || num1 >= num2) {
-        console.log(num1 ** num2);
-    } else if (typeof num1 === "string" || typeof num2 === "string") {
-        console.log("Rentre un num sale escroc");
+        return num1 ** num2;
     } else {
-        console.log("J'en ai marre, tu sais pas rentrer des nombres");
+        return "J'en ai marre, tu sais pas rentrer des nombres";
     }
 }
-console.log(aireCercle(3, 5));
+console.log(aireCercle(7, 5));
