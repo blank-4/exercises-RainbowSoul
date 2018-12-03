2d373813537e4c1aa90bbf13cb08538a88c6ef6f
BC029 <rainbow-life@outlook.fr>
2018-12-03 16:51:45 +0100

    Exercice 11

---
+++ javascript/lesson4/exercise11/exercice/script.js
@@ -0,0 +1,7 @@
+function arrays(array1, array2) {
+    array1 = ["1", "2"]; // Premier tableau.
+    array2 = ["3", "4"]; // Second tableau.
+    arraymerged = array1.concat(array2); // Fusion des deux tableaux.
+    console.log(arraymerged);
+}
+arrays();
