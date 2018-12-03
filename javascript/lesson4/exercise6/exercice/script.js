492ce82e94a84e7521352855041716765baed37a
BC029 <rainbow-life@outlook.fr>
2018-12-03 14:50:17 +0100

    Exercice 6 - Correction ( Fonctionne maintenant dans les deux sens )

--- javascript/lesson4/exercise6/exercice/script.js
+++ javascript/lesson4/exercise6/exercice/script.js
@@ -3,23 +3,24 @@ function chiffrer(string, caesar) {
    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
    var letters = string.split('');
    for (var i = 0; i < letters.length; i++) {
-      console.log(letters[i].split(''));
+      var result = letters[i];
+      console.log(result.split(''));
    }
    console.log('----- Message Encodé -----');
    for (var i = 0; i < letters.length; i++) {
-      var encoded = (alphabet.indexOf(letters[i]) + caesar) % 26;
-      console.log(alphabet[encoded].split(''));
+      var display = alphabet.indexOf(letters[i]) + caesar;
+      display = (display + 26) % 26;
+      console.log(alphabet[display].split('').join());
    }
 }
 
-chiffrer("hello", 2);
-// khoor = hello ( décalage de 3 )
-// jgnnq = hello ( décalage de 2 )
+chiffrer("je suis vivant", 2);
+// lgbuwkubxkxcpv = je suis vivant ( décalage de 2 )
 console.log('----- Message Codé Envoyé -----');
 
 function dechiffrer(string, caesar) {
    chiffrer(string, caesar);
 }
-dechiffrer("jgnnq", -2);
+dechiffrer("lgbuwkubxkxcpv", -2);
 
-// Je pense que ce que j'ai fais ici ne sert à rien.
+// Ceci dit, les espaces sont remplacés par des Z
