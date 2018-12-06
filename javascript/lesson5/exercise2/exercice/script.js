87bf9d477c0350dbfb3a971c2ca05b62dfa09129
BC029 <rainbow-life@outlook.fr>
2018-12-06 14:31:12 +0100

    Exercice 2

---
+++ javascript/lesson5/exercise2/exercice/script.js
@@ -0,0 +1,11 @@
+function whenCalled() {
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.style.width = "200px";
+    div.style.height = "400px";
+    div.style.backgroundColor = "white";
+    div.onmouseover = function() {
+        div.style.backgroundColor = "blue";
+    }
+}
+whenCalled();
