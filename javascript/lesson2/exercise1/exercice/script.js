c7480649a3eabe28d64cbf430013d82d1faec99f
Belkheiri Amine <30414257+RainbowSoul@users.noreply.github.com>
2018-12-10 10:51:20 +0100

    js lesson 2 (#22)
    * Exercice 1
    
    * Exercice 2
    
    * Exercice 3
    
    * Exercice 4
    
    * Exercice 5
    
    * All Five - Correction ( Suppression des espaces inutiles
    
    * Exercice 4 - Correction ( Indentation )

---
+++ javascript/lesson2/exercise1/exercice/script.js
@@ -0,0 +1,9 @@
+var heure = 12;
+
+if (heure < 10) {
+    console.log("C'est le matin");
+} else if ( heure < 20 ) {
+    console.log("C'est l'après-midi");
+} else {
+    console.log("C'est le soir");
+}
