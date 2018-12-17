4556ebec865c0709b3ab75a8fc6a09a7eefdb106
BC029 <rainbow-life@outlook.fr>
2018-12-17 16:02:56 +0100

    Exercice 1

---
+++ javascript/lesson7/exercise1/exercice/script.js
@@ -0,0 +1,12 @@
+var vue = new Vue({
+    el: '#app',
+    data: function() {
+        return {
+            profs: {
+                Alex: ['Lundi','Mardi','Mercredi','Vendredi'],
+                Erwan: ['Vendredi'],
+                Marin: ['Lundi','Mardi','Mercredi','Jeudi']
+            }
+        }
+    }
+});
