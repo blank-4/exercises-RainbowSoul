6dcd035808825cc5a70cb5d0776891f229f9e16c
BC029 <rainbow-life@outlook.fr>
2019-01-14 11:38:16 +0100

    Exercice 2 & 3 - Leçon 1 Node.js

---
+++ nodejs/lesson1/exercise3/npm app/index.js
@@ -0,0 +1,8 @@
+console.log('Hello World !');
+
+/* Suite à la fin du programme, je constate la création d'un fichier package.json */
+
+/* On constate qu'un dossier node_modules a été crée et le fichier package.json a été mis à jour ( ajout de dependencies ) */
+
+var chalk = require('chalk');
+console.log(chalk.red('Hello World !'));
